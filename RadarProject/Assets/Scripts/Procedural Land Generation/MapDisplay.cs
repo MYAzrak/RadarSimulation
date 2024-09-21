e8707e10f9d2de502c0ffb18e1b71cb7e9dcccfd
MohammadYA <yaserazrak5@gmail.com>
2024-09-22 01:13:25 +0400

    Created the Mesh of a Noise Map

--- RadarProject/Assets/Scripts/Procedural Land Generation/MapDisplay.cs
+++ RadarProject/Assets/Scripts/Procedural Land Generation/MapDisplay.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRenderer; // A ref to the renderer of the plane we created
+    public MeshFilter meshFilter; // Link b/2 Mesh and Renderer
+    public MeshRenderer meshRenderer;
 
     public void DrawTexture(Texture2D texture)
     {
         // textureRenderer.material // instantiated at runtime
         textureRenderer.sharedMaterial.mainTexture = texture;
         textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height); // Set the size of the plane = size of the map
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh(); // Shared since we could generate the mesh outside game mode
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 }
--- RadarProject/Assets/Scripts/Procedural Land Generation/MapGenerator.cs
+++ RadarProject/Assets/Scripts/Procedural Land Generation/MapGenerator.cs
@@ -11,7 +11,8 @@ public class MapGenerator : MonoBehaviour
     public enum DrawMode
     {
         NoiseMap,
-        ColorMap
+        ColorMap,
+        Mesh
     };
     public DrawMode drawMode;
 
@@ -51,6 +52,7 @@ public class MapGenerator : MonoBehaviour
         MapDisplay display = FindObjectOfType<MapDisplay>();
         if (drawMode == DrawMode.NoiseMap) display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
         else if (drawMode == DrawMode.ColorMap) display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        else if (drawMode == DrawMode.Mesh) display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
     }
 
     // Called whenever any script variables changes in the inspector
