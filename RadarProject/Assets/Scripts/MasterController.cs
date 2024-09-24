1617301b9a1878ecaf3620fc015ca416d185db32
yal77 <y.alhosani12@gmail.com>
2024-09-24 17:58:53 +0400

    Fixed issue with master controller

--- RadarProject/Assets/Scripts/MasterController.cs
+++ RadarProject/Assets/Scripts/MasterController.cs
@@ -9,12 +9,12 @@ public class MasterController : MonoBehaviour
     CSVController csvController;
     ScenarioController scenarioController;
 
+
     void Start()
     {
         radarController = GetComponent<RadarController>();
         csvController = GetComponent<CSVController>();
         scenarioController = GetComponent<ScenarioController>();
-
 
         string[] args = System.Environment.GetCommandLineArgs();
         Debug.Log("CLI Arg Length :" + args.Length);
@@ -26,43 +26,61 @@ public class MasterController : MonoBehaviour
         csvController.GenerateParameters();
 
         //CSV Controller Params
-        int.TryParse(getArg("-nships"), out csvController.numberOfShips);
-        int.TryParse(getArg("-nLocations"), out csvController.locationsToCreate);
-        float.TryParse(getArg("-minStartingCoords"), out csvController.minStartingCoordinates);
-        float.TryParse(getArg("-maxStartingCoords"), out csvController.maxStartingCoordinates);
-        float.TryParse(getArg("-randomCoords"), out csvController.randomCoordinates);
-        int.TryParse(getArg("-minSpeed"), out csvController.minSpeed);
-        int.TryParse(getArg("-maxSpeed"), out csvController.maxSpeed);
-
+        setIntArg("-nships", ref csvController.numberOfShips);
+        setIntArg("-nLocations", ref csvController.locationsToCreate);
+        setFloatArg("-minStartingCoords", ref csvController.minStartingCoordinates);
+        setFloatArg("-maxStartingCoords", ref csvController.maxStartingCoordinates);
+        setFloatArg("-randomCoords", ref csvController.randomCoordinates);
+        setIntArg("-minSpeed", ref csvController.minSpeed);
+        setIntArg("-maxSpeed", ref csvController.maxSpeed);
 
         //Radar Params
-        int.TryParse(getArg("-radarRows"), out radarController.rows);
+        setIntArg("-radarRows", ref radarController.rows);
 
         int nRadars = 0;
-        if (int.TryParse(getArg("-nRadars"), out nRadars))
+        if (setIntArg("-nRadars", ref nRadars))
         {
-            for (var x = 0; x < nRadars; x++)
-            {
-                radarController.GenerateRadar();
-            }
+            radarController.GenerateRadars(nRadars);
         }
 
         //Scenario Params
         int nScenarios = 0;
-        if (int.TryParse(getArg("-nScenarios"), out nScenarios))
+        if (setIntArg("-nScenarios", ref nScenarios))
         {
             //TODO: refactor scenario controller UI code to seperate method, callable from here
             //TODO: after generating scenarios automatically load them
+            csvController.GenerateScenarios(nScenarios);
+            scenarioController.LoadAllScenarios();
         }
 
         //TODO: Start the simulation
-
-
     }
 
     void Update()
     {
 
+    }
+
+    private bool setIntArg(string argName, ref int parameter)
+    {
+        string arg = getArg(argName);
+        if (arg != null && int.TryParse(arg, out int value))
+        {
+            parameter = value;
+            return true;
+        }
+        return false;
+    }
+
+    private bool setFloatArg(string argName, ref float parameter)
+    {
+        string arg = getArg(argName);
+        if (arg != null && float.TryParse(arg, out float value))
+        {
+            parameter = value;
+            return true;
+        }
+        return false;
     }
 
     private static string getArg(string name)
